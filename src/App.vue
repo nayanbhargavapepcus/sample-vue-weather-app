3ddcb2031ed10eacfbbf55120a51b221ebd0716b
nayanbhargavapepcus <nayan.bhargava@pepcus.com>
2023-09-26 14:50:16 -0700

    creating api key from open weather and search box for our app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
-  <img alt="Vue logo" >
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search..."/>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
 
 export default {
   name: 'App',
+  data () {
+    return {
+      api_key: '740c0ae88d540e85dc5e6e0e21be35b6'
+    }
+  }
 }
 </script>
 
